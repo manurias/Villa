067901fb89acb98132439505f152f4dbe8673a0c
Manuel Arias <67820427+manurias@users.noreply.github.com>
2020-08-10 00:12:59 +0200

    Create pikomon.js

---
+++ pikomon.js
@@ -0,0 +1,13 @@
+class pikomon
+{
+  constructor(n)
+  {
+  this.nome = n;
+  this.tipo = "terra";
+  }
+ } 
+  
+  var muca = new pikomon("muca");
+  var pollo = new pikomon("pollo");
+  console.log(muca);
+  console.log(pollo);
