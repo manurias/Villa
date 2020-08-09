5b4ab0f9bb244269f579962d586826baa007b795
Manuel Arias <67820427+manurias@users.noreply.github.com>
2020-08-10 00:13:34 +0200

    Create pikmon.js

---
+++ pikmon.js
@@ -0,0 +1,13 @@
+class pikmon
+{
+  constructor(n)
+  {
+  this.nome = n;
+  this.tipo = "terra";
+  }
+ } 
+  
+  var muca = new pikmon("muca");
+  var pollo = new pikmon("pollo");
+  console.log(muca);
+  console.log(pollo);
