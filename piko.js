0616fa3c7fe895e679a10617cbaa388588ae6f8f
Manuel Arias <67820427+manurias@users.noreply.github.com>
2020-08-11 00:42:34 +0200

    update and rename pikmon.js to pike.js

--- piko.js
+++ piko.js
@@ -7,7 +7,7 @@ class pikmon
   }
  } 
   
-  var muca = new pikmon("muca");
-  var pollo = new pikmon("pollo");
+  var muca = new pikmon("muca", 100, 30);
+  var pollo = new pikmon("pollo", 80, 50);
   console.log(muca);
   console.log(pollo);
