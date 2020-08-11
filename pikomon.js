968a9e9f2e1f43e94fa5d54f0410a5b2a1c37385
Manuel Arias <67820427+manurias@users.noreply.github.com>
2020-08-12 01:11:18 +0200

    Update pikomon.js

--- pikomon.js
+++ pikomon.js
@@ -1,13 +1,25 @@
 class pikomon
 {
-  constructor(n)
+  constructor(n, v, a)
   {
-  this.nome = n;
-  this.tipo = "terra";
+    this.imagine = new Image();
+    this.nome = n;
+    this.vità = v;
+    this.attacco = a;
+    
+    this.imagine.src = imagini[this.nome];
   }
+  parlare()
+  {
+    alert(this.nome);
  } 
-  
-  var muca = new pikomon("muca");
-  var pollo = new pikomon("pollo");
-  console.log(muca);
-  console.log(pollo);
+  mostrare()
+  {
+    document.body.appendChild(this.imagine);
+    document.write("<br /><strong>" +
+  this.nome + </strong><br />");
+    document.write("vità: " + this.vità + "<br />);
+    document.write("attacco: " + this.attacco + "<hr />");
+  }
+}
+ 
